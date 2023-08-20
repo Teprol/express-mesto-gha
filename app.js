3e938ef81e05825a694ea501ee011de205800876
Sergey <teprol112@bk.ru>
2023-08-20 05:29:04 +0400

    fix/автотесты1

--- app.js
+++ app.js
@@ -3,14 +3,13 @@ const mongoose = require('mongoose');
 // eslint-disable-next-line import/no-extraneous-dependencies
 const cookieParser = require('cookie-parser');
 // eslint-disable-next-line import/no-extraneous-dependencies
-const { err } = require('celebrate');
+const { errors } = require('celebrate');
 const usersRout = require('./routes/users');
 const cardsRout = require('./routes/cards');
 const authRout = require('./routes/auth');
 const auth = require('./middlewares/auth');
 const errHandller = require('./middlewares/errHandller');
-// eslint-disable-next-line import/no-unresolved, import/extensions
-const { NotFoundError } = require('../errors/NotFoundError');
+const { NotFoundError } = require('./errors/NotFoundError');
 
 const app = express();
 
@@ -34,7 +33,7 @@ app.use('/*', (req, res, next) => {
   next(new NotFoundError('Страница не найдена'));
 });
 // центр. обработка ошибок
-app.use(err());
+app.use(errors());
 app.use(errHandller);
 
 app.listen(PORT);
